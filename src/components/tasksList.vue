a04e485aa5d12527986ea89e5b24872d1fccb766
oinot <antonio.ferraz.coutinho@gmail.com>
2022-09-17 18:04:15 +0200

    some tweaks

--- src/components/tasksList.vue
+++ src/components/tasksList.vue
@@ -4,7 +4,11 @@
       <h4 class="card-header">Tasks</h4>
       <div class="card-body">
       <label for="task" class="input-group d-flex">
-        <div class="input-group m-1" v-for="task in tasks" :key="task.id">
+        <div class="input-group mb-2" v-for="task in tasks" :key="task.id">
+          <!-- <input
+          class="form-check-input m-2 "
+          type="checkbox" value=""
+          aria-label="Checkbox for following text input"> -->
           <input
           :class="[task.is_complete ? 'toggledTask' : '']"
           class="form-control"
@@ -19,12 +23,12 @@
           aria-describedby="button-addon3">
           Done
           </button>
-          <button
-          @click.stop="useEditTask(task.id)"
+          <!-- <button
+          @click.stop="useEditTask(task.title, task.id)"
           class="btn btn-dark"
           type="button">
           Edit
-          </button>
+          </button> -->
           <button
           @click.prevent="useDeleteTask(task.id)"
           class="btn btn-danger"
@@ -33,6 +37,12 @@
           </button>
         </div>
       </label>
+      <button
+          @click.stop="useEditTask(tasks.title, tasks.id)"
+          class="btn btn-outline-secondary px-3 m-1 float-end"
+          type="button">
+          Edit Tasks
+        </button>
       </div>
     </div>
   </section>
@@ -70,11 +80,12 @@ export default {
     useDeleteTask(id) {
       this.deleteTask(id);
     },
-    // useEditTask() {
-    //   this.readOnly = !this.readOnly;
-    //   this.disabled = !this.disabled;
-    //   this.newEditedTitle = this.editTask(this.newEditedTitle);
-    // },
+    useEditTask(title, id) {
+      this.readOnly = !this.readOnly;
+      this.disabled = !this.disabled;
+      this.newEditedTitle = title;
+      this.editTask(title, id);
+    },
     useToggleTask(isComplete, id) {
       this.toggleTask(isComplete, id);
     },
